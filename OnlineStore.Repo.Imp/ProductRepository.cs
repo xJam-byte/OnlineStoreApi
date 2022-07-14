cf4b3b14d1c932fe019cf55846cb018772a41eec
Your Name <you@example.com>
2022-07-14 21:41:15 +0600

    added base product repository

---
+++ OnlineStore.Repo.Imp/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using OnlineStore.Model;
+using OnlineStore.Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Repo.Impl
+{
+  public class ProductRepository : IProductRepository 
+  {
+    public async Task DeleteProduct(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public async Task<Product> GetProductById(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public async Task<List<Product>> GetProducts(int? top = null)
+    {
+      throw new NotImplementedException();
+    }
+
+    public async Task<int> InsertProduct(Product product)
+    {
+      throw new NotImplementedException();
+    }
+
+    public async Task UpdateProduct(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ OnlineStore.Repo.Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnlineStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Repo.Interfaces
+{
+  public interface IProductRepository
+  {
+    Task<Product> GetProductById(int id);
+    Task<List<Product>> GetProducts(int? top = null);//? - может и принимать и не принимать
+    Task<int> InsertProduct(Product product);
+    Task UpdateProduct(int id);
+    Task DeleteProduct(int id);
+
+  }
+}
